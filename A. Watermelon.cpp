4e110fa544b03a0e4108f5bfcb28bbebee8f7d3f
HasibDCL <hasib@deepchainlabs.com>
2023-08-18 14:17:21 +0600

    C++

---
+++ A. Watermelon.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/4/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+
+    cin>>w;
+
+    (w%2 || w==2) ? cout<<"No"<< endl : cout<<"YES"<< endl;
+}
