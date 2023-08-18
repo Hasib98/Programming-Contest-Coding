5721d1674e59ed40e0b86d24d8f53c74635b5265
HasibDCL <hasib@deepchainlabs.com>
2023-08-18 18:39:08 +0600

    Python

---
+++ IKEYB - I-Keyboard.py
@@ -0,0 +1,18 @@
+#https://www.spoj.com/problems/IKEYB/
+using namespace std;
+int main()
+{
+    int t,Count = 0;
+    bool Petya, Vasya , Tonya ;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>> Petya >>  Vasya  >> Tonya ;
+        if(  (Petya && (Vasya || Tonya)) || ((Petya || Vasya) && Tonya)  ){
+            Count++;
+        }
+    }
+    cout<<Count<<endl;
+}
